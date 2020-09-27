71fc81a887efa86db6c4382c654c4fd381af9354
HamishWHC <hamishwhc@gmail.com>
2020-09-27 13:44:17 +1000

    Added colour restrictions and optimised some things.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ColorResult, GithubPicker } from 'react-color';
 import './App.css';
 import { FirebaseContext } from './firebase';
+
+const PIXEL_SIZE = 10;
 
 interface RPlacePixel {
   user: string,
@@ -10,74 +12,111 @@ interface RPlacePixel {
   y: number
 }
 
-type RPlaceImage = RPlacePixel[]
+const COLOUR_PALETTE = [
+  "#FFFFFF",
+  "#E4E4E4",
+  "#888888",
+  "#222222",
+  "#FFA7D1",
+  "#E50000",
+  "#E59500",
+  "#A06A42",
+  "#E5D900",
+  "#94E044",
+  "#02BE01",
+  "#00D3DD",
+  "#0083C7",
+  "#0000EA",
+  "#CF6EE4",
+  "#820080"
+]
+
+type RPlaceImage = Map<string, RPlacePixel>
+
+interface UserData {
+  lastPlacementTimestamp: Date
+}
+
+const getMousePosition = (canvas: HTMLCanvasElement, event: MouseEvent) => {
+  let rect = canvas.getBoundingClientRect();
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+  return [x, y]
+}
 
 function App() {
   const [userId, setUserId] = React.useState<string | null>(null)
   const [imageData, setImageData] = React.useState<RPlaceImage | null>(null)
-  const [colour, setColour] = React.useState("#00FF00")
+  const [colour, setColour] = React.useState(COLOUR_PALETTE[5])
+  const canvas = React.useRef<HTMLCanvasElement>(null)
   const firebase = React.useContext(FirebaseContext)
+  const [userData, setUserData] = React.useState<UserData | null>()
+
   React.useEffect(() => {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        setUserId(user.uid)
-      } else {
-        setUserId(null)
-      }
-    });
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => setUserId(user?.uid ?? null));
     firebase.auth().signInAnonymously()
+    return unsubscribe
   }, [firebase])
 
   React.useEffect(() => {
-    firebase.firestore().collection("pixels").onSnapshot((snapshot) => {
-      setImageData(snapshot.docs.map(doc => doc.data() as RPlacePixel))
-    })
-    for (let x = 0; x < 25; x++) {
-      for (let y = 0; y < 25; y++) {
-        let doc = firebase.firestore().collection("pixels").doc(`${x}-${y}`)
-        doc.get().then(snapshot => {
-          if (!snapshot.exists) {
-            doc.set({
-              x, y, colour: "#FFFFFF"
-            })
-          }
-        })
-      }
+    if (userId) {
+      const unsubscribe = firebase.firestore().collection("users").doc(userId).onSnapshot(snapshot => setUserData(snapshot.data() as UserData))
+      return unsubscribe
+    } else {
+      setUserData(null)
     }
+  }, [userId, firebase])
+
+  React.useEffect(() => {
+    const unsubscribe = firebase.firestore().collection("pixels").onSnapshot((snapshot) => {
+      // For each change in the snapshot, add the change to imageData, then change the reference (by passing to Map) to trigger a React state update.
+      setImageData(new Map(snapshot.docChanges().reduce(
+        (acc, change) => acc.set(change.doc.id, change.doc.data() as RPlacePixel),
+        imageData ?? new Map<string, RPlacePixel>()
+      )))
+    })
+    return unsubscribe
+    // imageData should not be included here as we do not want the effect run (which would duplicate the snapshot handler) whenever the image updates.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [firebase])
 
-  const pixelAdder = React.useCallback((x, y) => {
-    return () => {
-      firebase.firestore().collection("pixels").doc(`${x}-${y}`).set({
-        x, y,
-        colour,
-        user: userId
-      })
-    }
+  React.useEffect(() => {
+    if (!canvas.current || !imageData) return
+    const ctx = canvas.current?.getContext("2d")
+    if (!ctx) return
+    imageData.forEach(pixel => {
+      ctx.fillStyle = pixel.colour
+      ctx.fillRect(pixel.x * PIXEL_SIZE, pixel.y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE)
+    })
+  }, [imageData])
+
+  const addPixel = React.useCallback((event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (!canvas.current) return
+    const [x, y] = getMousePosition(canvas.current, event.nativeEvent).map(a => Math.floor(a / PIXEL_SIZE))
+    firebase.firestore().collection("pixels").doc(`${x}-${y}`).set({
+      x, y,
+      colour,
+      user: userId
+    })
   }, [colour, firebase, userId])
+
+  const handlerColourChangeComplete = React.useCallback((colour: ColorResult, _event: React.ChangeEvent<HTMLInputElement>) => {
+    setColour(colour.hex)
+    console.log(colour)
+  }, [])
 
   return (
     <>
-      <h1>r/Place Clone</h1>
+      <h1>r/Place Clone</h1><GithubPicker color={colour} onChangeComplete={handlerColourChangeComplete} triangle="hide" colors={COLOUR_PALETTE} />
       {
-        imageData && <div style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(25, 20px)`,
-          gridTemplateRows: `repeat(25, 20px)`
+        imageData ? <canvas width={1000} height={1000} onMouseDown={addPixel} ref={canvas} style={{border: "1px solid black"}} /> : <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: 1000,
+          height: 1000
         }}>
-          {imageData.map(pixel => <div
-            key={`${pixel.x}-${pixel.y}`}
-            style={{
-              width: "100%",
-              height: "100%",
-              backgroundColor: pixel.colour,
-              gridColumnStart: pixel.x + 1,
-              gridColumnEnd: pixel.x + 2,
-              gridRowStart: pixel.y + 1,
-              gridRowEnd: pixel.y + 2
-            }}
-            onClick={pixelAdder(pixel.x, pixel.y)}
-          />)}
+          Loading...
         </div>
       }
     </>
